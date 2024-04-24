88f25b410345f8e743d5a654c0ee852726c9dd1f
Gokhan Kus <gkus1998@hotmail.com>
2024-04-24 19:23:30 +0300

    fibo series

--- SortingAlgorithm/Program.cs
+++ SortingAlgorithm/Program.cs
@@ -246,93 +246,107 @@ namespace SortingAlgorithm
             //    }
             //    Console.WriteLine();
             //}
+            #region BlackJack
+            //string response = string.Empty;
 
+            //int jack = 10;
+            //int queen = 10;
+            //int king = 10;
+            //int ace = 11;
 
+            //int[] sayilar = { 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace };
+            //Random rnd = new Random();
 
-            string response = string.Empty;
+            //int elimdekilerToplami = 0;
+            //int gelenSayi = 0;
+            //int bilgisayarinEli = 0;
+            //string bilgisayarinKartlari = "";
+            //do
+            //{
+            //    for (int i = 0; i < 4; i++)
+            //    {
+            //        if (i < 2)
+            //        {
+            //            gelenSayi = rnd.Next(0, sayilar.Length);
+            //            elimdekilerToplami += sayilar[gelenSayi];
+            //            Console.WriteLine($"Kartlarim: {sayilar[gelenSayi]}");
+            //        }
+            //        else
+            //        {
+            //            while (bilgisayarinEli <= 11)
+            //            {
+            //                gelenSayi = rnd.Next(0, sayilar.Length);
+            //                bilgisayarinKartlari += gelenSayi + ", ";
+            //                bilgisayarinEli += sayilar[gelenSayi];
+            //            }
+            //        }
+            //    }
 
-            int jack = 10;
-            int queen = 10;
-            int king = 10;
-            int ace = 11;
+            //    Console.WriteLine("Kart cek: Y\nsonucu gor: any tus");
 
-            int[] sayilar = { 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace };
+            //    response = Console.ReadLine();
+            //    do
+            //    {
+            //        if (response == "y".ToLower() || response == "Y".ToLower())
+            //        {
+            //            gelenSayi = rnd.Next(0, sayilar.Length);
+            //            Console.WriteLine("cektigin kart: " + sayilar[gelenSayi]);
+            //            elimdekilerToplami += sayilar[gelenSayi];
+            //            if (elimdekilerToplami > 21)
+            //            {
+            //                Console.WriteLine("elindekilerin toplami: " + elimdekilerToplami + " GAMEOVER");
+            //                break;
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("elindekiler toplami: " + elimdekilerToplami);
+            //                Console.WriteLine("bir kart daha cekmek ister misin? E/H");
+            //                response = Console.ReadLine();
+            //            }
+            //        }
+            //        else
+            //        {
+            //            if (bilgisayarinEli >= elimdekilerToplami)
+            //            {
+            //                Console.WriteLine($"Bilgisayarin Kartlari: {bilgisayarinKartlari}");
+            //                Console.WriteLine($"Bilgisayarin eli: {bilgisayarinEli} >= {elimdekilerToplami} GAMEOVER");
+            //                break;
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine($"elimdekiler toplami: {elimdekilerToplami} > bilgisayarin eli{bilgisayarinEli} KAZANDIM");
+            //                break;
+            //            }
+            //        }
+            //    } while (response != "n");
 
-            Random rnd = new Random();
+            //    Console.WriteLine("1 el daha oynamak ister misin? E/H");
 
-            int elimdekilerToplami = 0;
-            int gelenSayi = 0;
-            int bilgisayarinEli = 0;
-            string bilgisayarinKartlari = "";
-            do
+            //    response = Console.ReadLine();
+
+            //    elimdekilerToplami = 0;
+            //    bilgisayarinEli = 0;
+
+            //} while (response != "H".ToLower());
+            #endregion
+
+            //fibonacci 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 ...
+            int firstOne = 1;
+            int secondOne = 1;
+            List<int> fiboList = new List<int>();
+            for (int i = 1; i < 20; i++)
             {
-                for (int i = 0; i < 4; i++)
-                {
-                    if (i < 2)
-                    {
-                        gelenSayi = rnd.Next(0, sayilar.Length);
-                        elimdekilerToplami += sayilar[gelenSayi];
-                        Console.WriteLine($"Kartlarim: {sayilar[gelenSayi]}");
-                    }
-                    else
-                    {
-                        while (bilgisayarinEli <= 11)
-                        {
-                            gelenSayi = rnd.Next(0, sayilar.Length);
-                            bilgisayarinKartlari += gelenSayi + ", ";
-                            bilgisayarinEli += sayilar[gelenSayi];
-                        }
-                    }
-                }
+                //Console.Write($"{firstOne}, {secondOne}, ");
+                fiboList.Add(firstOne);
+                fiboList.Add(secondOne);
+                firstOne = secondOne + firstOne;
+                secondOne = firstOne + secondOne;
+            }
 
-                Console.WriteLine("Kart cek: Y\nsonucu gor: any tus");
-
-                response = Console.ReadLine();
-                do
-                {
-                    if (response == "y".ToLower() || response == "Y".ToLower())
-                    {
-                        gelenSayi = rnd.Next(0, sayilar.Length);
-                        Console.WriteLine("cektigin kart: " + sayilar[gelenSayi]);
-                        elimdekilerToplami += sayilar[gelenSayi];
-                        if (elimdekilerToplami > 21)
-                        {
-                            Console.WriteLine("elindekilerin toplami: " + elimdekilerToplami + " GAMEOVER");
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("elindekiler toplami: " + elimdekilerToplami);
-                            Console.WriteLine("bir kart daha cekmek ister misin? E/H");
-                            response = Console.ReadLine();
-                        }
-                    }
-                    else
-                    {
-                        if (bilgisayarinEli >= elimdekilerToplami)
-                        {
-                            Console.WriteLine($"Bilgisayarin Kartlari: { bilgisayarinKartlari}");
-                            Console.WriteLine($"Bilgisayarin eli: {bilgisayarinEli} >= {elimdekilerToplami} GAMEOVER");
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine($"elimdekiler toplami: {elimdekilerToplami} > bilgisayarin eli{bilgisayarinEli} KAZANDIM");
-                            break;
-                        }
-                    }
-                } while (response != "n");
-
-                Console.WriteLine("1 el daha oynamak ister misin? E/H");
-
-                response = Console.ReadLine();
-
-                elimdekilerToplami = 0;
-                bilgisayarinEli = 0;
-
-            } while (response != "H".ToLower());
-
-
+            foreach (var fiboTerm in fiboList)
+            {
+                Console.WriteLine(fiboTerm);
+            }
             Console.ReadLine();
         }
 
