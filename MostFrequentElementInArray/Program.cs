ebf5b73789d73565aab893958588509a15de18d3
Gokhan Kus <gkus1998@hotmail.com>
2024-05-19 18:11:01 +0300

    Most Frequent Element In Array

---
+++ MostFrequentElementInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MostFrequentElementInArray
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string text = "assdddadasss";
+			Dictionary<string, int> duplicateSayisi = new Dictionary<string, int>();
+			for (int i = 0; i < text.Length; i++)
+			{
+				int tekrarEdenElemanSayisi = 0;
+				for (int j = 0; j < text.Length; j++)
+				{
+					if (text[i] == text[j])
+					{
+						tekrarEdenElemanSayisi++;
+						duplicateSayisi.TryAdd(text[i].ToString(),tekrarEdenElemanSayisi);
+						duplicateSayisi[text[i].ToString()] = tekrarEdenElemanSayisi;
+					}
+				}
+			}
+			foreach (var harf in duplicateSayisi)
+			{
+                Console.WriteLine($"{harf}");
+            }
+			
+			Console.ReadLine();
+		}
+	}
+}
--- SumsOfNumber'sDigit/Program.cs
+++ SumsOfNumber'sDigit/Program.cs
@@ -4,15 +4,29 @@
 	{
 		static void Main(string[] args)
 		{
-			int sayi = 25367;
-			int rakamlarToplam = 0;
+			//int sayi = 25367;
+			//int rakamlarToplam = 0;
+			//while (sayi != 0)
+			//{
+			//	int kalan = sayi % 10;
+			//	rakamlarToplam += kalan;
+			//	sayi /= 10;
+			//}
+			//Console.WriteLine(rakamlarToplam);
+
+			int sayi = 25243;
+			string s = sayi.ToString();
+			int basamakSayisi = s.Length - 1;
+			int basamakToplam = 0;
 			while (sayi != 0)
 			{
-				int kalan = sayi % 10;
-				rakamlarToplam += kalan;
-				sayi /= 10;
+				int bolen = Convert.ToInt32(Math.Pow(10, basamakSayisi)); 
+				int basamakDeger = (sayi) / bolen;  
+				sayi = sayi - (basamakDeger * bolen);  
+				basamakToplam += basamakDeger;
+				basamakSayisi--;
 			}
-			Console.WriteLine(rakamlarToplam);
-		}
+            Console.WriteLine(basamakToplam);
+        }
 	}
 }
